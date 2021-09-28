5a234ab620f253662a6e249599562f988d05457d
hekaahve <heini.k.ahven@student.jyu.fi>
2021-09-28 20:54:57 +0300

    to Heroku

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ app.use(express.json())
 
   })
   
-  const port = process.env.port || 3001
-  app.listen(port)
-  console.log(`Server running on port ${port}`)
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
